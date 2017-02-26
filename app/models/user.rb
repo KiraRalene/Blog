19414ccbf5d6d00e4e58eb8b34893aa04ad51995
Kira Gross <kira.ralene@gmail.com>
2017-02-25 20:14:57 -0800

    added validations and relationships to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,23 @@
 class User < ApplicationRecord
+  has_secure_password
+  has_many :posts, dependent: :nullify
+  has_many :comments, dependent: :nullify
+
+  before_validation :downcase_email
+
+VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+validates :email, presence: true, uniqueness: true,
+          format: VALID_EMAIL_REGEX
+
+def full_name
+  "#{first_name} #{last_name}".strip.titleize
+end
+
+private
+
+def downcase_email
+  self.email&.downcase!
+end
+
+
 end
