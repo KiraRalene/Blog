c89c3bc745982b80f44ba8d3bdcb4ca7ffea7d08
Kira Gross <kira.ralene@gmail.com>
2017-02-25 21:42:50 -0800

    created new.html.erb for users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,13 +6,13 @@ class UsersController < ApplicationController
   def create
     user_params = params.require(:user).permit(:first_name,
                                                :last_name,
-                                               :email
+                                               :email,
                                                :password,
                                                :password_confirmation)
-    @use = User.new user_params
+    @user = User.new user_params
     if @user.save
       session[:user_id] = @user_id
-      redirect_to root_path, notice: "Logged In"
+      redirect_to posts_path, notice: "Logged In"
     else
       render :new
     end
